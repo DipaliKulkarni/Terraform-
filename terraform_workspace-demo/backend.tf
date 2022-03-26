ee8ce8940ad6157d6462948d87897012a8241739
Dipali Kulkarni <96540459+DipaliKulkarni@users.noreply.github.com>
2022-03-26 21:20:10 +0530

    Update backend.tf

--- terraform_workspace-demo/backend.tf
+++ terraform_workspace-demo/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "ctl-terraform-state-ap-south-1"
+    bucket         = "demo-bucket-02"
     key            = "tfdemo/workspace"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-state"
+    dynamodb_table = "demo-backend"
   }
 }
