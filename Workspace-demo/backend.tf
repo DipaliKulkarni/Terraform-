8ab7e41865d4e11e43ec81aa869ec687eb5f1c1d
Dipali Kulkarni <96540459+DipaliKulkarni@users.noreply.github.com>
2022-03-26 21:00:05 +0530

    Delete Workspace-demo directory

--- Workspace-demo/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket = "dips-devops-bucket-tf"
-        key = "workspace-demo/dev-state-file"
-        region = "ap-south-1"
-        dynamodb_table = "terraform-state"
-      
-    }
-}
